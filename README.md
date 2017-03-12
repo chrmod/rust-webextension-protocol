4e6c98a48c4b38762c1af0fa3ff865f191222e3f
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-03-12 22:35:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository is indended to create a template for projects that want to run rust binaries from web browser with use of native massaging api.
+
+At current stage it serves as playground - please do not consider it as anything serious.
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("{} bytes read", n);
+            println!("{}", input);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+}
+
