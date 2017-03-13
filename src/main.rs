094d90e6e6c0462231f8c61af70e1190a92cc021
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-03-13 22:47:54 +0100

    NativeMessaging protocol parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use std::io;
+use std::str;
+use std::io::Read;
+
+extern crate byteorder;
+
+use std::io::Cursor;
+use byteorder::{LittleEndian, ReadBytesExt};
 
 fn main() {
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            println!("{} bytes read", n);
-            println!("{}", input);
-        }
-        Err(error) => println!("error: {}", error),
+    loop {
+        // Read JSON size
+        let mut buffer = [0; 4];
+        io::stdin().read_exact(&mut buffer);
+        let mut buf = Cursor::new(&buffer);
+        let size = buf.read_u32::<LittleEndian>().unwrap();
+        println!("size: {:?}", size);
+
+        // Read JSON
+        let mut data_buffer = vec![0u8; size as usize];
+        io::stdin().read_exact(&mut data_buffer);
+        let s = str::from_utf8(&data_buffer).unwrap();
+        println!("data: {}", s);
     }
 }
-
