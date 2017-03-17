0de19c5a46d3916dd66acb1069ccf8965f571d41
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-03-17 19:19:48 +0100

    reading from stdin or a file

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,53 @@
 use std::io;
 use std::str;
 use std::io::Read;
+use std::io::Stdin;
+use std::fs::File;
 
 extern crate byteorder;
 
 use std::io::Cursor;
 use byteorder::{LittleEndian, ReadBytesExt};
 
-fn main() {
-    loop {
-        // Read JSON size
-        let mut buffer = [0; 4];
-        io::stdin().read_exact(&mut buffer);
-        let mut buf = Cursor::new(&buffer);
-        let size = buf.read_u32::<LittleEndian>().unwrap();
-        println!("size: {:?}", size);
+enum Input {
+    File(File),
+    Stdin(Stdin),
+}
 
-        // Read JSON
-        let mut data_buffer = vec![0u8; size as usize];
-        io::stdin().read_exact(&mut data_buffer);
-        let s = str::from_utf8(&data_buffer).unwrap();
-        println!("data: {}", s);
+impl Read for Input {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match *self {
+            Input::File(ref mut file) => file.read(buf),
+            Input::Stdin(ref mut stdin) => stdin.read(buf),
+        }
     }
+}
+
+fn read(mut input: Input) -> String {
+    // Read JSON size
+    let mut buffer = [0; 4];
+    input.read_exact(&mut buffer).expect("cannot read size");
+    let mut buf = Cursor::new(&buffer);
+    let size = buf.read_u32::<LittleEndian>().unwrap();
+    println!("size: {:?}", size);
+
+    // Read JSON
+    let mut data_buffer = vec![0u8; size as usize];
+    input.read_exact(&mut data_buffer).expect("cannot read data");
+    let string = str::from_utf8(&data_buffer).unwrap().to_string();
+    println!("data: {}", string);
+    return string;
+}
+
+fn main() {
+    /*
+    loop {
+        let f = Input::Stdin(io::stdin());
+        read(f);
+    }
+    */
+    let f = Input::File(File::open("myfile").unwrap());
+    read(f);
+    /*
+    */
 }
